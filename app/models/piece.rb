1b49f61e06765f1e054f7ecc62f8d4e98a57eccf
Lindy Sinclair <lindyrp@gmail.com>
2016-05-16 16:50:17 +0000

    Update piece.rb to original code

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,11 +1,3 @@
 class Piece < ActiveRecord::Base
 
-  attr_reader :board, :color
-  attr_accessor :position
-  
-  def initialize(position, board, color)
-    @position = position
-    @board = board
-    @color = color
-  end
 end
